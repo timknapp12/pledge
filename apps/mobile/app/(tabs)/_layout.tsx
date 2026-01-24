82889afa74e64e582b729a99159be68322723816
Tim Knapp <timknapp12@gmail.com>
2026-01-24 00:28:00 -0700

    make custom tab bar

--- apps/mobile/app/(tabs)/_layout.tsx
+++ apps/mobile/app/(tabs)/_layout.tsx
@@ -1,39 +1,17 @@
 import React from 'react';
-import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { Tabs } from 'expo-router';
-import { useTheme } from 'styled-components/native';
-
-function TabBarIcon(props: {
-  name: React.ComponentProps<typeof FontAwesome>['name'];
-  color: string;
-}) {
-  return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
-}
+import { CustomTabBar } from '@/components';
 
 export default function TabLayout() {
-  const theme = useTheme();
-
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: theme.colors.tint,
         headerShown: false,
       }}
+      tabBar={(props) => <CustomTabBar {...props} />}
     >
-      <Tabs.Screen
-        name='index'
-        options={{
-          title: 'Tab One',
-          tabBarIcon: ({ color }) => <TabBarIcon name='code' color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name='two'
-        options={{
-          title: 'Tab Two',
-          tabBarIcon: ({ color }) => <TabBarIcon name='code' color={color} />,
-        }}
-      />
+      <Tabs.Screen name="index" />
+      <Tabs.Screen name="two" />
     </Tabs>
   );
 }
--- apps/mobile/components/common/index.ts
+++ apps/mobile/components/common/index.ts
@@ -3,3 +3,4 @@ export * from './containers';
 export * from './buttons';
 export * from './inputs';
 export * from './ThemeSelector';
+export * from './CustomTabBar';
