dd247862e80528221621b0cb3bd7edf1b66af159
Tim Knapp <timknapp12@gmail.com>
2026-01-23 20:43:55 -0700

    add styled components for themes

--- apps/mobile/app/(tabs)/two.tsx
+++ apps/mobile/app/(tabs)/two.tsx
@@ -1,31 +1,30 @@
-import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
+import styled from 'styled-components/native';
 
 export default function TabTwoScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/two.tsx" />
-    </View>
+    <Container>
+      <Title>Tab Two</Title>
+      <Separator />
+    </Container>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
+const Container = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  background-color: ${(props) => props.theme.colors.background};
+`;
+
+const Title = styled.Text`
+  font-size: 20px;
+  font-weight: bold;
+  color: ${(props) => props.theme.colors.text};
+`;
+
+const Separator = styled.View`
+  margin: 30px 0;
+  height: 1px;
+  width: 80%;
+  background-color: ${(props) => props.theme.colors.separator};
+`;
