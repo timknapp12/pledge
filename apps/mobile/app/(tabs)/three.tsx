fe63698db4f23030cd3d9ebca3f3524705787b4c
Tim Knapp <timknapp12@gmail.com>
2026-01-24 00:32:28 -0700

    add a third tab

---
+++ apps/mobile/app/(tabs)/three.tsx
@@ -0,0 +1,9 @@
+import { Title2, ScreenContainer } from '@/components';
+
+export default function TabThreeScreen() {
+  return (
+    <ScreenContainer>
+      <Title2>Profile</Title2>
+    </ScreenContainer>
+  );
+}
